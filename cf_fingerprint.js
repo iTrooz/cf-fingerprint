707952ee242320052f407c5c01b4521f464b00e2
iTrooz <hey@itrooz.fr>
2025-07-27 13:55:17 +0200

    perf: use Math.imul() instead of slow BigInt

--- cf_fingerprint.js
+++ cf_fingerprint.js
@@ -4,60 +4,47 @@
  * https://github.com/arschedev/cf-fingerprint
  */
 
-export function cf_fingerprint(buffer) {
-	const multiplex = uint32_t(1540483477)
-	const length = uint32_t(buffer.length)
+export function cf_fingerprint(rawBuffer) {
+  const multiplex = 1540483477;
 
-	const num1 = uint32_t(compute_normalized_length(buffer))
+  const filteredBuffer = filterWhitespace(rawBuffer);
+  let filteredLength = filteredBuffer.length;
+  let fingerprint = (1 ^ filteredLength) >>> 0;
 
-	let num2 = uint32_t(1) ^ num1
-	let num3 = uint32_t(0)
-	let num4 = uint32_t(0)
+  let chunkFingerprint = 0;
+  let chunkAcc = 0;
+  for (let i = 0; i < filteredLength; i++) {
+    chunkFingerprint |= filteredBuffer[i] << chunkAcc;
+    chunkAcc += 8;
 
-	for (let index = uint32_t(0); index < length; index++) {
-		const byte = uint32_t(buffer[index])
+    // Once 4 bytes (32 bits) are accumulated, process chunk
+    if (chunkAcc === 32) {
+      let num6 = Math.imul(chunkFingerprint, multiplex) >>> 0;
+      let num7 = Math.imul(num6 ^ (num6 >>> 24), multiplex) >>> 0;
 
-		if (!is_whitespace_character(byte)) {
-			num3 |= byte << num4
-			num4 += uint32_t(8)
-			if (num4 === uint32_t(32)) {
-				const num6 = uint32_t(num3 * multiplex)
-				const num7 = uint32_t((num6 ^ (num6 >> uint32_t(24))) * multiplex)
+      fingerprint = (Math.imul(fingerprint, multiplex) ^ num7) >>> 0;
 
-				num2 = uint32_t((num2 * multiplex) ^ num7)
-				num3 = uint32_t(0)
-				num4 = uint32_t(0)
-			}
-		}
-	}
+      chunkFingerprint = 0;
+      chunkAcc = 0;
+    }
+  }
 
-	if (num4 > 0) {
-		num2 = uint32_t((num2 ^ num3) * multiplex)
-	}
+  // process last chunk
+  if (chunkAcc > 0) {
+    fingerprint = (Math.imul(fingerprint ^ chunkFingerprint, multiplex)) >>> 0;
+  }
 
-	const num6 = uint32_t((num2 ^ (num2 >> uint32_t(13))) * multiplex)
+  let num6 = Math.imul(fingerprint ^ (fingerprint >>> 13), multiplex) >>> 0;
+  let result = num6 ^ (num6 >>> 15);
 
-	// result
-	return parseInt(uint32_t(num6 ^ (num6 >> uint32_t(15))))
+  return result >>> 0;
+}
 
-	function uint32_t(num) {
-		return BigInt.asUintN(32, BigInt(num))
-	}
-
-	function compute_normalized_length(buffer) {
-		let num1 = 0
-		const length = buffer.length
-
-		for (let index = 0; index < length; index++) {
-			if (!is_whitespace_character(buffer[index])) {
-				num1++
-			}
-		}
-
-		return num1
-	}
-
-	function is_whitespace_character(b) {
-		return b == 9 || b == 10 || b == 13 || b == 32
-	}
+function filterWhitespace(buffer) {
+  const ws = new Set([9, 10, 13, 32]);
+  const filtered = [];
+  for (let i = 0; i < buffer.length; i++) {
+    if (!ws.has(buffer[i])) filtered.push(buffer[i]);
+  }
+  return filtered;
 }
