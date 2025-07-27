18d43d32017a52b220025d6a70ee08894fd617de
iTrooz <hey@itrooz.fr>
2025-07-27 15:02:20 +0200

    remove all >>> 0

--- cf_fingerprint.js
+++ cf_fingerprint.js
@@ -9,7 +9,7 @@ export function cf_fingerprint(rawBuffer) {
 
   const filteredBuffer = filterWhitespace(rawBuffer);
   let filteredLength = filteredBuffer.length;
-  let fingerprint = (1 ^ filteredLength) >>> 0;
+  let fingerprint = (1 ^ filteredLength);
 
   let chunkFingerprint = 0;
   let chunkAcc = 0;
@@ -19,10 +19,10 @@ export function cf_fingerprint(rawBuffer) {
 
     // Once 4 bytes (32 bits) are accumulated, process chunk
     if (chunkAcc === 32) {
-      let num6 = Math.imul(chunkFingerprint, multiplex) >>> 0;
-      let num7 = Math.imul(num6 ^ (num6 >>> 24), multiplex) >>> 0;
+      let num6 = Math.imul(chunkFingerprint, multiplex);
+      let num7 = Math.imul(num6 ^ (num6 >>> 24), multiplex);
 
-      fingerprint = (Math.imul(fingerprint, multiplex) ^ num7) >>> 0;
+      fingerprint = (Math.imul(fingerprint, multiplex) ^ num7);
 
       chunkFingerprint = 0;
       chunkAcc = 0;
@@ -31,13 +31,13 @@ export function cf_fingerprint(rawBuffer) {
 
   // process last chunk
   if (chunkAcc > 0) {
-    fingerprint = (Math.imul(fingerprint ^ chunkFingerprint, multiplex)) >>> 0;
+    fingerprint = (Math.imul(fingerprint ^ chunkFingerprint, multiplex));
   }
 
-  let num6 = Math.imul(fingerprint ^ (fingerprint >>> 13), multiplex) >>> 0;
+  let num6 = Math.imul(fingerprint ^ (fingerprint >>> 13), multiplex);
   let result = num6 ^ (num6 >>> 15);
 
-  return result >>> 0;
+  return result;
 }
 
 function filterWhitespace(buffer) {
