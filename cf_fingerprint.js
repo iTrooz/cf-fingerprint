9652783fb52cee6662ce51ed5fb1f1402df51101
arschedev <98528463+arschedev@users.noreply.github.com>
2025-07-30 05:45:41 +0300

    Format

--- cf_fingerprint.js
+++ cf_fingerprint.js
@@ -1,57 +1,61 @@
 /**
- * cf-fingerprint v0.1.0
+ * cf-fingerprint v0.2.0
  * GNU General Public License v3.0
  * https://github.com/arschedev/cf-fingerprint
  */
 
 export function cf_fingerprint(rawBuffer) {
-  const multiplex = 1540483477;
+	const multiplex = 1540483477
 
-  const filteredBuffer = filterWhitespace(rawBuffer);
-  let filteredLength = filteredBuffer.length;
-  let fingerprint = 1 ^ filteredLength;
+	const filteredBuffer = filterWhitespace(rawBuffer)
+	let filteredLength = filteredBuffer.length
+	let fingerprint = 1 ^ filteredLength
 
-  let chunkFingerprint = 0;
-  let chunkAcc = 0;
-  for (let i = 0; i < filteredLength; i++) {
-    chunkFingerprint |= filteredBuffer[i] << chunkAcc;
-    chunkAcc += 8;
+	let chunkFingerprint = 0
+	let chunkAcc = 0
 
-    // Once 4 bytes (32 bits) are accumulated, process chunk
-    if (chunkAcc === 32) {
-      let num6 = Math.imul(chunkFingerprint, multiplex);
-      let num7 = Math.imul(num6 ^ (num6 >>> 24), multiplex);
+	for (let i = 0; i < filteredLength; i++) {
+		chunkFingerprint |= filteredBuffer[i] << chunkAcc
+		chunkAcc += 8
 
-      fingerprint = (Math.imul(fingerprint, multiplex) ^ num7);
+		// process chunk once 4 bytes (32 bits) are accumulated
+		if (chunkAcc === 32) {
+			let num6 = Math.imul(chunkFingerprint, multiplex)
+			let num7 = Math.imul(num6 ^ (num6 >>> 24), multiplex)
 
-      chunkFingerprint = 0;
-      chunkAcc = 0;
-    }
-  }
+			fingerprint = Math.imul(fingerprint, multiplex) ^ num7
 
-  // process last chunk
-  if (chunkAcc > 0) {
-    fingerprint = (Math.imul(fingerprint ^ chunkFingerprint, multiplex));
-  }
+			chunkFingerprint = 0
+			chunkAcc = 0
+		}
+	}
 
-  let num6 = Math.imul(fingerprint ^ (fingerprint >>> 13), multiplex);
-  let result = (num6 ^ (num6 >>> 15)) >>> 0;
+	// process last chunk
+	if (chunkAcc > 0) {
+		fingerprint = Math.imul(fingerprint ^ chunkFingerprint, multiplex)
+	}
 
-  return result;
+	let num6 = Math.imul(fingerprint ^ (fingerprint >>> 13), multiplex)
+	let result = (num6 ^ (num6 >>> 15)) >>> 0
+
+	return result
 }
 
-// filter buffer in-place
+// Filter buffer in-place
 function filterWhitespace(buffer) {
-  const ws = new Set([9, 10, 13, 32]);
-  let currentIndex = 0;
-  for (let i = 0; i < buffer.length; i++) {
-    if (!ws.has(buffer[i])) {
-      // write non-whitespace byte to the current index
-      if (currentIndex !== i) {
-        buffer[currentIndex] = buffer[i];
-      }
-      currentIndex++;
-    }
-  }
-  return buffer.subarray(0, currentIndex);
+	const ws = new Set([9, 10, 13, 32])
+	let currentIndex = 0
+
+	for (let i = 0; i < buffer.length; i++) {
+		if (!ws.has(buffer[i])) {
+			// write non-whitespace byte to the current index
+			if (currentIndex !== i) {
+				buffer[currentIndex] = buffer[i]
+			}
+
+			currentIndex++
+		}
+	}
+
+	return buffer.subarray(0, currentIndex)
 }
